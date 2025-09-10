256c2abb7a6ae83c19dd229340990c907bd0f3d9
Product Team at Netcore Cloud <Productops@netcorecloud.com>
2025-09-10 11:42:21 +0530

    Add styles for campaign module

--- src/pages/Campaigns.tsx
+++ src/pages/Campaigns.tsx
@@ -1,11 +1,17 @@
 import { AppLayout } from '@/components/layout/AppLayout';
 import { CampaignsPage } from '@/components/campaigns/CampaignsPage';
+import './Campaigns.css';
 
 const Campaigns = () => {
   return (
-    <AppLayout>
-      <CampaignsPage />
-    </AppLayout>
+    <section
+      id="campaign-module"
+      className="w-screen max-w-none overflow-x-hidden"
+    >
+      <AppLayout>
+        <CampaignsPage />
+      </AppLayout>
+    </section>
   );
 };
 
